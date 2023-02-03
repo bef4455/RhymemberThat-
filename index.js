8e84f73d71d771886581061af43b554b0a2e13ed
bef4455 <121890688+bef4455@users.noreply.github.com>
2023-02-03 08:55:04 +0100

    Add files via upload
    Signed-off-by: bef4455 <121890688+bef4455@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
+
+
+
 window.addEventListener("keydown", function (event) {
     if (event.code === "Enter") {
-        document.querySelector("#level-title").style.display = "none";
-        startGame()
-        
-    }
+        document.querySelector("#level-title").style.visibility = "hidden";
+    // enter.play();
+}
+startGame();   
 })
 
-let pink = document.querySelector('#pink')
-let blue = document.querySelector('#blue')
-let green = document.querySelector('#green')
-let purple = document.querySelector('#purple')
+let blueWav = new Audio('sound/blue.wav')
+let greenWav = new Audio('sound/green.wav')
+let pinkWav = new Audio('sound/pink.wav')
+let purpleWav = new Audio('sound/purple.wav')
+let wrong = new Audio('sound/wrong.wav')
+let enter = new Audio('sound/enter.wav')
+const tryAgain = document.querySelector('.btn-try-again')
+let pink = document.querySelector('.pink')
+let blue = document.querySelector('.blue')
+let green = document.querySelector('.green')
+let purple = document.querySelector('.purple')
+
+
+let gameOverElement = document.querySelector(".game-over")
+let scoreElement = document.querySelector('#score')
 let lights = [pink, blue, green, purple]
-let level = 0;
+let level = 1;
 let sequence = [];
+let clickCount = 0
+let score = 0
 let intervalId
 
 
-function startGame() {
-    // let userChoseColour = document.getElementById("level-title");
-    // userClick.push()
-    generateSequence(4)
-    console.log(sequence)
+
+
+function reset() {
+level = 1
+sequence = []
+clickCount = 0
+score = 0
+scoreElement.textContent = score
+clearInterval(intervalId)
+startGame()
 }
 
+
+function startGame() {
+    generateSequence(2)
+    // console.log(sequence)
+
+}
+
+
+
+
 function generateSequence(num) {
+    sequence = []
+  
     for (let i = 0; i < num; i++) {
         const randomIndex = Math.floor(Math.random() * lights.length);
         const randomLight = lights[randomIndex]
@@ -33,17 +66,86 @@ function generateSequence(num) {
     intervalId = setInterval(() => {
         const currentElement = sequence[iteration]
         currentElement.classList.add('active')
+       
+
+
+        if (pink.classList.contains("active")){
+            pinkWav.play();}
+
+            if (green.classList.contains("active")){
+                greenWav.play(); } 
+
+                if (blue.classList.contains("active")){
+                    blueWav.play();}
+
+                    if (purple.classList.contains("active")){
+                        purpleWav.play()}
+
+                       
+
         setTimeout(() => {
             currentElement.classList.remove('active')
-        }, 400)
+        }, 600)
         iteration++
         if (iteration === sequence.length) {
             clearInterval(intervalId)
+            playersTurn()
         }
-    }, 500)
+    }, 900)
 }
-// console.log(sequence)
-// generateSequence(4)
-// console.log(sequence)
 
-// console.log(activLights.map(sequence => activLights.length));
+
+pink.addEventListener("click", function(){
+    pinkWav.play()
+})
+green.addEventListener("click", function(){
+    greenWav.play()
+})
+blue.addEventListener("click", function(){
+    blueWav.play()
+})
+purple.addEventListener("click", function(){
+    purpleWav.play()
+})
+
+function playersTurn() {
+    lights.forEach(button => {
+        button.addEventListener("click", handleClick)
+    })
+}
+
+function handleClick(event) {
+    if (sequence[clickCount] === event.target) {
+        
+        clickCount++
+        score+=10
+        console.log('nice', score)
+        scoreElement.textContent = score
+        if (sequence.length === clickCount) {
+            // console.log('YESSSSS')
+            clickCount = 0
+            level++
+            generateSequence(1.4 * level)
+        }
+    } else {
+        wrong.play();
+      gameOver();
+
+    //    console.log("loose")
+}}
+ 
+
+ function gameOver() {
+gameOverElement.style.display = "block"
+tryAgain.addEventListener("click",()=>{
+    
+    tryAgain.classList.add('remove')
+    
+    gameOverElement.style.display = "none"
+    console.log("yes a wantoutraille")
+    reset();
+    
+})
+
+}
+        
